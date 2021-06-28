1d9eb7e42d38791605a2ccf6cdbb44bfb04eee0a
amajesticpotatoe <amajesticpotatoe@gmail.com>
2021-06-27 20:36:14 -0400

    refactor: add spacer to toolbar

--- packages/vuer-fe/components/index-page/treeview-pane-toolbar.vue
+++ packages/vuer-fe/components/index-page/treeview-pane-toolbar.vue
@@ -1,12 +1,19 @@
 <template>
   <v-toolbar color="grey darken-4" dense>
+    <template v-for="(action, name) in actions">
+    
+    <v-spacer
+      v-if="action.is === 'v-spacer'"
+      :key="`action-${name}`"
+    />
+
     <component
-      v-for="(action, name) in actions"
+      v-else
       :key="`action-${name}`"
       :is="action.is"
-      v-bind="{ ...action }"
+      v-bind="action"
     />
-      
+    </template>
   </v-toolbar>
 </template>
 
@@ -26,6 +33,7 @@ export default {
     const actions = reactive({
       open: { is: 'outline-dialog' },
       close: { is: 'toolbar-btn', clickAction: closeOutline, disabled: disabledClose, icon: 'mdi-book-remove', tooltip: 'Close Outline' },
+      spacer: { is: 'v-spacer' },
       expandAll: { is: 'toolbar-btn', icon: 'mdi-expand-all', tooltip: 'Expand All' },
       collapseAll: { is: 'toolbar-btn', icon: 'mdi-collapse-all', tooltip: 'Collapse All' },
     })
