2dcc5f0c9375bdf6f5af8c6ae81aefd0529e78c4
captaindav <dave@answerglobal.com>
2022-03-14 18:54:07 -0400

    Added clean.sh removed clean.js

--- scripts/clean.js
+++
@@ -1,2 +0,0 @@
-find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
-find . -name 'package-lock.json' -type d -print -exec rm -rf '{}' \;
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,2 +1,2 @@
 find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
-find . -name 'package-lock.json' -type f -print -exec rm -rf '{}' \;
+find . -name 'package-lock.json' -type d -print -exec rm -rf '{}' \;
