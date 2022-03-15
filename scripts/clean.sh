7cea950e2d9f4f2b7942b2ddf29b45752be270e7
captaindav <dave@answerglobal.com>
2022-03-15 14:05:17 -0400

    fixed script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-findnm="find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' ;"
-findpl="find . -name 'package-lock.json' -type f -prune -print "
-
-${findnm}
-${findpl}
-
-
+find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' + ;
+find . -name 'package-lock.json' -type f -print -exec rm -rf '{}' + ;
 
 
