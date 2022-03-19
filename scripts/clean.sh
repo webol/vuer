f93707380b1fa69ba03bde5ffca6e6ac13ea3921
Dave Connerth <captaindav@gmail.com>
2022-03-19 14:05:23 -0400

    Added .DS_Store to clean.sh.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' + ;
+find . -name '.nuxt' -type d -prune -print -exec rm -rf '{}' + ;
+
 find . -name 'package-lock.json' -type f -print -exec rm -rf '{}' + ;
 find . -name 'yarn.lock' -type f -print -exec rm -rf '{}' + ;
-
-
+find . -name '.DS_Store' -type f -print -exec rm -rf '{}' + ;
