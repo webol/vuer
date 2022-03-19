6f035becdb16a607a39d03a163aa4b9257ccc88f
Dave Connerth <captaindav@gmail.com>
2022-03-19 13:56:15 -0400

    Delete yarn.lock

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' + ;
 find . -name 'package-lock.json' -type f -print -exec rm -rf '{}' + ;
+find . -name 'yarn.lock' -type f -print -exec rm -rf '{}' + ;
 
 
